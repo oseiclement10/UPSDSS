bfc5a3e894f82d0e3816906f05ca084d467a4c68
Clement Osei Asante <clemzymillz@gmail.com>
2022-08-09 11:21:13 +0000

    front end validation of program selection

---
+++ Public/js/main.js
@@ -0,0 +1,21 @@
+let form = document.getElementById("courseform");
+let boxes = document.getElementsByClassName("opt");
+
+function checkSelected(){
+    let count = 0;
+    for(let i=0; i<boxes.length; i++){
+        if(boxes[i].checked==true){
+            count++;
+        }
+    }
+    return count;
+}
+form.addEventListener("submit",e=>{
+    if(checkSelected()==0){
+        e.preventDefault();
+        alert("You Must Select At least 1 Program");
+    }else if(checkSelected()>1){
+        e.preventDefault();
+        alert("Select Only 1 program")
+    }
+})
