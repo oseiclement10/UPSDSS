855d044051de32e9be02ad453ecc5ae370cb421b
Clement Osei Asante <clemzymillz@gmail.com>
2022-08-18 13:17:34 +0000

    used grades for annotation on graph

--- Public/js/chart.js
+++ Public/js/chart.js
@@ -62,6 +62,33 @@ function getColor(grade){
     }
 }
 
+function getGrade(grade){
+        switch(grade){
+            case 1:
+             return "A1";
+            case 2: 
+             return "B2";
+            case 3:
+             return "B3";
+            case 4:
+             return "C4";
+            case 5:
+             return "C5";
+            case 6:
+             return "C6";
+            case 7:
+             return "D7";    
+            case 8:
+             return "E8"; 
+            case 9:
+             return "F9";
+            default :
+             return 0;
+    
+        }
+}
+
+
 fetch('/users/grades',{
     method:"GET"
 }).then(response=>response.json()).then(
@@ -71,9 +98,10 @@ fetch('/users/grades',{
         newdata.map(elem=>{
             elem.push(getScore(parseInt(elem[1])));
             elem.push(getColor(parseInt(elem[1])));
+            elem.push(getGrade(parseInt(elem[1])));
         })
 
-    
+        console.log(newdata);
         google.charts.setOnLoadCallback(()=>drawChart(newdata));
     }
     
@@ -85,14 +113,14 @@ fetch('/users/grades',{
 function drawChart(data) {
     var data = google.visualization.arrayToDataTable([
     ["Subject", "Score", { role: "style" },{role:'annotation'} ],
-    [`${data[0][0]}`,data[0][2],`color:${data[0][3]};margin:10px`,`${data[0][2]}`],
-    [`${data[1][0]}`,data[1][2],`color:${data[1][3]};margin:10px`,`${data[1][2]}`],
-    [`${data[2][0]}`,data[2][2],`color:${data[2][3]};margin:10px`,`${data[2][2]}`],
-    [`${data[3][0]}`,data[3][2],`color:${data[3][3]};margin:10px`,`${data[3][2]}`],
-    [`${data[4][0]}`,data[4][2],`color:${data[4][3]};margin:10px`,`${data[4][2]}`],
-    [`${data[5][0]}`,data[5][2],`color:${data[5][3]};margin:10px`,`${data[5][2]}`],
-    [`${data[6][0]}`,data[6][2],`color:${data[6][3]};margin:10px`,`${data[6][2]}`],
-    [`${data[7][0]}`,data[7][2],`color:${data[7][3]};margin:10px`,`${data[7][2]}`]
+    [`${data[0][0]}`,data[0][2],`color:${data[0][3]};margin:10px`,`${data[0][4]}`],
+    [`${data[1][0]}`,data[1][2],`color:${data[1][3]};margin:10px`,`${data[1][4]}`],
+    [`${data[2][0]}`,data[2][2],`color:${data[2][3]};margin:10px`,`${data[2][4]}`],
+    [`${data[3][0]}`,data[3][2],`color:${data[3][3]};margin:10px`,`${data[3][4]}`],
+    [`${data[4][0]}`,data[4][2],`color:${data[4][3]};margin:10px`,`${data[4][4]}`],
+    [`${data[5][0]}`,data[5][2],`color:${data[5][3]};margin:10px`,`${data[5][4]}`],
+    [`${data[6][0]}`,data[6][2],`color:${data[6][3]};margin:10px`,`${data[6][4]}`],
+    [`${data[7][0]}`,data[7][2],`color:${data[7][3]};margin:10px`,`${data[7][4]}`]
     
     // ["GKA",45,'color:brown',"E8"]
     ]);
