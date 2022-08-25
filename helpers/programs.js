812f0ff3087fa9201e6758693de639c9df008dad
Clement Osei Asante <clemzymillz@gmail.com>
2022-08-25 00:56:55 +0000

    added agriculture to programs , added notes to interest

--- helpers/programs.js
+++ helpers/programs.js
@@ -4,5 +4,6 @@ module.exports = courses = {
     "Business":['Elective Mathematics','Accounting','Economics','Business Management', 'Costing'],
     "Visual Arts":['Picture Making','Textiles','Graphics','Elective Mathematics','Economics','GKA(general knowledge in arts)','Leather','Sculpture'],
     "Home Economics":['Management In Living','Food and Nuitrition','Clothing and Textiles','Biology','Chemistry','Economics'],
-    "Technical":['Building Construction','Physics','Wood','Elective Mathematics','Technical Drawing']
+    "Technical":['Building Construction','Physics','Wood','Elective Mathematics','Technical Drawing'],
+    "Agriculture":["General Agriculture","Crop Science","Animal Science","Chemistry","Physics","Elective Mathematics"]
 }
